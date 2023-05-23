0b2178b4a4ae7c59b4b6f5293d86169d74c6daea
aditya meena <adityameena@thoughtwin.com>
2023-05-23 13:01:23 +0530

    testing for auto trigger 2.4

--- test_pipeline_pro/settings.py
+++ test_pipeline_pro/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#testing 2.4
+#testing 2.5
