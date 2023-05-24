0601b84b2006f9b65eb93fbf8ee3ae66269abd6a
aditya meena <adityameena@thoughtwin.com>
2023-05-24 13:19:02 +0530

    this is new pull

--- jenkinsapp/urls.py
+++ jenkinsapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from jenkinsapp.views import create_student
 
 urlpatterns = [
-    path('',create_student,name = 'create-student')
+    path('', create_student,name = 'create-student'),
 ]
 
